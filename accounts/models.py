b1012f79987cf371414ccc72bf3eed577ff63d24
Luke Mikael Dones <lukemikaeldones@Lukes-iMac.local>
2017-10-31 12:29:56 +0800

    accounts -> models has post save feature for additional details

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    USER_TYPE_CHOICE = (
+    	('materialplanning', 'materialplanning'),
+    	('productdevelopment', 'productdevelopment'),
+    	('production', 'production'),
+    	('admin', 'admin')
+    )
+    user_type = models.CharField(max_length=100, choices=USER_TYPE_CHOICE, default='admin')
     description = models.CharField(max_length=100, default='')
     phone = models.IntegerField(default=0)
+
+    def __str__(self):
+    	return "username:" + self.user.username + "     pk:" + str(self.pk)
+
+def create_profile(sender, **kwargs):
+	if kwargs['created']: #true
+		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
