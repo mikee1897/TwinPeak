699ca8d0ab20075b3acf413a315c7ac4918a6125
Luke Mikael <lukeatthetree@hotmail.com>
2017-10-31 22:34:54 +0800

    accounts login views update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-
+	url(r'^$', views.authentication), #serve view depending on user && check if logged in or not
+#	url(r'^login/$', views.login),
+#	url(r'^logout/$', views.logout),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-# Create your views here.
+def homepage_redirect(request):
+	user_type = request.user.userprofile.user_type
+	if user_type == 'productdevelopment':
+		pass
+	elif user_type == 'materialplanning':
+		pass
+	elif user_type == 'production':
+		pass
+	elif user_type == 'admin':
+		pass
+
+def login(request):
+	username = request.POST('username')
+	password = request.POST('password')
+	user = authenticate(request, username=username, password=password)
+	user_type = user.userprofile.user_type
+	if user is not None:
+		login(request, user)
+	else:
+		pass
+
+def logout(request):
+	logout(request)
+	return redirect('/')
